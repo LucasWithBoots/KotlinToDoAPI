3d97369e77f230ab0872c05adfde4fd5296be43c
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-06-29 13:45:12 -0300

    feat: validação de DTOs usando @field

--- src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/dto/AtualizarStatusTarefaDTO.kt
+++ src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/dto/AtualizarStatusTarefaDTO.kt
@@ -1,8 +1,15 @@
 package io.github.lucaswithboots.kotlintodoapi.dto
 
 import io.github.lucaswithboots.kotlintodoapi.model.StatusTarefa
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
+import org.jetbrains.annotations.NotNull
 
 data class AtualizarStatusTarefaDTO(
+    @field:NotNull
+    @field:Positive
     val id: Long,
+
+    @field:NotBlank
     val status: StatusTarefa
 )
