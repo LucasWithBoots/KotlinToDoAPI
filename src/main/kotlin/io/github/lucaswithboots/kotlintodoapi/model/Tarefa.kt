27398f561ab2860482f2a22b58b0d923e38cb159
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-07-01 19:39:17 -0300

    fix: implementação do application.properties e configurando jda nos models

--- src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/model/Tarefa.kt
+++ src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/model/Tarefa.kt
@@ -1,12 +1,17 @@
 package io.github.lucaswithboots.kotlintodoapi.model
 
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
+@Entity
 data class Tarefa(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
     val titulo: String,
     val descricao: String,
     val dataDeCriacao: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val usuario: Usuario,
+    @Enumerated(EnumType.STRING)
     var status: StatusTarefa = StatusTarefa.PENDENTE
 )
--- src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/model/Usuario.kt
+++ src/main/kotlin/io/github/lucaswithboots/kotlintodoapi/model/Usuario.kt
@@ -1,8 +1,14 @@
 package io.github.lucaswithboots.kotlintodoapi.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import java.time.LocalDateTime
 
+@Entity
 data class Usuario(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
     val nome: String,
     val email: String,
